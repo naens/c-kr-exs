8b33da01389ff89c1bc53d663d29e8825dcdbd27
naens <naens@gmx.com>
2017-10-19 20:41:02 +0200

    chapter4: calculator (not finished yet)

--- ch4/calculator.c
+++ ch4/calculator.c
@@ -1,100 +1,172 @@
 #include <stdio.h>
-#include <math.h>
+#include <string.h>
 
-/* In order to be able to include this project in my repository I decided to
- * write my own version of the calculator and not take any code from the
- * book.
- *
- * Specifications
- *  + user enters input line by line
- *  + each line is an expression in poish notation
- *  + the stack and the stack pointer are global variables
- *  + the stack is represented as an array
- *  + the stack is accessible through push and pop functions
- *  + supported operations: +, -, *, /, %, sin, exp, pow
- *  + the numbers are in signed int or in double notation
- *  + write functions getch and ungetch, that work with a buffer (!!EOF!!)
- *  + write function ungets that works with strings
- */
+#define END_ELEMENT 0
+#define END_LINE 98
+#define END_INPUT 123
 
-struct elm {
-  int typ;
-  int val;
-};
+#define ELEM_OPER 110
+#define ELEM_NUMBER 111
+#define ELEM_VAR 112
 
-char *read_elm()
+#define OPER_PLUS 11
+#define OPER_MINUS 12
+#define OPER_MULT 17
+#define OPER_DIV 13
+#define OPER_MOD 14
+#define OPER_EXP 15
+#define OPER_LOG 16
+
+#define NO_VAL 1110187
+
+
+int read_element(char *buf)
 {
-  static char buf[0x10000];
-  static int next_eol=0;
-  if (next_eol)
-    {
-      buf[0] = '\n';
-      buf[1] = 0;
-      next_eol = 0;
-    }
-  int chr = getchar();
-  while (chr == '\t' || chr == ' ')
-    chr = getchar();
+  int c;
   int i = 0;
-  while (1)
+  while (c = getchar())
     {
-      if (chr == '\n')
+      if (i == 0 && (c == ' ' || c == '\t'))
+        continue;
+      if (i > 0 && (c == ' ' || c == '\t'))
         {
-          /* if i = 0, return end-of-line */
-          if (i == 0)
-            {
-              buf[i++] = chr;
-              break;
-            }
-          /* if i > 0, return element, set next-end-of-line=true */
-          else
-            {
-              next_eol = 1;
-              break;
-            }
+          buf[i] = 0;
+          return END_ELEMENT;
         }
-      else if (chr == '\t' || chr == ' ')
+      if (c == EOF)
         {
-          next_eol = 0;
-          break;
+          buf[i] = 0;
+          return END_INPUT;
+        }
+      if (c == '\n')
+        {
+          buf[i] = 0;
+          return END_LINE;
         }
       else
-        buf[i++] = chr;
-      chr = getchar();
+        buf[i++] = c;
     }
-  buf[i] = 0;
-  return buf;
+  i--;
+  while (i > 0 && (buf[i] == ' ' || buf[i] == '\t'))
+    i--;
+  buf[i + 1] = 0;
+  return END_ELEMENT;
 }
 
-struct elm *parse_elm(char *c)
+char *trim(char *str)
 {
-  static struct elm elm;
-  return &elm;
+  int len = strlen(str);
+  char *c = str;
+  while (*c == ' ' || *c == '\t' || *c == ';')
+    c++;
+  char *d = &str[len - 1];
+  while (*d == ' ' || *d == '\t' || *d == ';')
+    d--;
+  *(d + 1) = 0;
+  return c;
 }
 
-void work_with(struct elm *elm)
+int is_number(char *str)
 {
+  while (*str >= '0' && *str <= '9')
+    str++;
+  return str == 0;
 }
 
-int pop()
+int read_number(char *str)
 {
+  int Result = 0;
+  while (*str)
+    {
+      Result *= 10;
+      Result += *str - '0';
+    }
+  return Result;
 }
+
+/* elements: numbers([0-9]+), operations(+,-,*,/,%,exp,log), variables(rest) */
+void parse_element(char *elem, int *type, int *value)
+{
+  if (strcmp(elem, "+") == 0)
+    {
+      *type = ELEM_OPER;
+      *value = OPER_PLUS;
+    }
+  else if (strcmp(elem, "-") == 0)
+    {
+      *type = ELEM_OPER;
+      *value = OPER_MINUS;
+    }
+  else if (strcmp(elem, "*") == 0)
+    {
+      *type = ELEM_OPER;
+      *value = OPER_MULT;
+    }
+  else if (strcmp(elem, "/") == 0)
+    {
+      *type = ELEM_OPER;
+      *value = OPER_DIV;
+    }
+  else if (strcmp(elem, "%") == 0)
+    {
+      *type = ELEM_OPER;
+      *value = OPER_MOD;
+    }
+  else if (strcmp(elem, "exp") == 0)
+    {
+      *type = ELEM_OPER;
+      *value = OPER_EXP;
+    }
+  else if (strcmp(elem, "log") == 0)
+    {
+      *type = ELEM_OPER;
+      *value = OPER_LOG;
+    }
+  else if (is_number(elem))
+    {
+      *type = ELEM_NUMBER;
+      *value = read_number(elem);
+    }
+  else
+    {
+      *type = ELEM_VAR;
+      *value = NO_VAL;
+    }
+}
+
+
+
 
 int main(int argc, char **argv)
 {
-  if (argc == 1)                /* stdin mode */
+  if (argc == 1)                /* read from stdin */
     {
-      struct elm *curr;
-      while ((curr = parse_elm(read_elm())))
+      int res;
+      char buf[0x100];
+      while ((res = read_element(buf)) != END_INPUT)
         {
-          printf("type=%d, value=%d\n", curr->typ, curr->val);
-
+          switch (res)
+            {
+            case END_ELEMENT:   /* work with element */
+              printf("element: '%s'\n", buf);
+              break;
+            case END_LINE:      /* send end of line to the calculator */
+              printf("end of line\n");
+              break;
+            case END_INPUT:     /* send end of input to the calculator */
+              printf("end of input\n");
+              break;
+            }
         }
     }
-  else                          /* read arguments, print result */
+  else                          /* read from argv */
     {
       for (int i = 1; i < argc; i++)
-        work_with(parse_elm(argv[i]));
-      printf("%d\n", pop());
+        {
+          char *el = trim(argv[i]);
+          printf("element=%s\n", el);
+          /* TODO: send element to calculator */
+        }
+      /* TODO: send end of input to calculator */
     }
 }
