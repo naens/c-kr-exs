197bc054492ff88dd8a2f209be9f612b52bc40a1
naens <naens@gmx.com>
2017-11-07 15:36:36 +0100

    ch6:miniplm: test if jq exists for tests

--- ch6/miniplm/runtests.sh
+++ ch6/miniplm/runtests.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
+jq=jq
+command -v $jq > /dev/null
+if [ "$?" -eq 1 ]
+then
+    echo command "'$jq'" does not exist
+    exit 1
+fi
+
 for f in tests/*.json
 do
     echo Testing $f
 
     #process line by line, compare
-    n=$(cat $f | jq '.[].start' | wc -l)
-    echo n=$n
+    n=$(cat $f | $jq '.[].start' | wc -l)
     i=0
     while [ $i -lt $n ]
     do
-        str="$(cat $f | jq -c '.['$i']')"
-	start=$(echo "$str" | jq .start)
-	input=$(echo "$str" | jq .input)
-	result=$(echo "$str" | jq -c .result)
+        str="$(cat $f | $jq -c '.['$i']')"
+	start=$(echo "$str" | $jq .start)
+	input=$(echo "$str" | $jq .input)
+	result=$(echo "$str" | $jq -c .result)
         res=$(eval ./parse_test $start $input 2>&1)
         if [ "$?" -ne 0 ] && [ "$result" = "bad input" ]
         then
