b35a5d924d9dedba8f3183504a57e51a41b11e57
naens <naens@gmx.com>
2017-11-10 00:43:06 +0100

    ch6:miniplm:tests update

---
+++ ch6/miniplm/testgen.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# test generator
+
+echo "Enter a module name:"
+read name
+str="["
+first=true
+while true
+do
+    if [ "$first" = "false" ]
+    then
+        str="$str,"
+    fi
+    echo "Enter a nonterminal:"
+    read start
+
+    echo "Enter an expression:"
+    read input
+
+    result=$(./parse_test "$start" "$input" 2>1)
+
+    echo start="$start" input="$input"
+    echo result="$result"
+
+    echo "Enter one more item(y/n)?"
+    read answer
+    echo "Your answer is \"$answer\""
+    if [ "$answer" = "n" ]
+    then
+        break
+    fi
+    first=false
+done
+
+echo "Finished entering tests for module $name"
+ 
