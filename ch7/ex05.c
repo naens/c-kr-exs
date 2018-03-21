8465dc79d32814e91a6355ac9f7df020c7212cfb
naens <naens@gmx.com>
2018-03-21 15:20:36 +0100

    ch7:ex05

---
+++ ch7/ex05.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void scan(char fmt, a* void)
+{
+    int c;
+    int i = 0;
+    char buf[0x100];
+    while ((c = getchar()) != EOF)
+    {
+        if (c == ' ' || c == '\t' || c == '\n')
+            continue;
+        buf[i++] = c;
+    }
+    buf[i] = 0;
+    if (i == 0)
+        *a = NULL;
+    else if (fmt == 'c')
+        *a = buf[0];
+    else
+        *a = atoi(buf);
+}
+
+int main(int argc, char **argv)
+{
+}
