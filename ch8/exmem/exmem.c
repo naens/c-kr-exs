254904f475b44bdca4649e870ac7bc13bcb4c102
naens <naens@gmx.com>
2018-03-27 11:44:47 +0200

    ch8:exmem: fix types

--- ch8/exmem/exmem.c
+++ ch8/exmem/exmem.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <stdint.h>
 
-typedef uint64_t tbl;
-#define B_ADD(p, n) ((p) + (n) * sizeof(tbl))
-#define B_ACCESS(b, n) (*(((tbl*)b) + n))
-#define B_SIZE(b) B_ACCESS(b, 0)
-#define B_NEXT(b) B_ACCESS(b, 1)
-#define B_PREV(b) B_ACCESS(b, 2)
-#define B_DATA(b) B_ADD(b, 3)
-#define B_FROM_DATA_PTR(p) B_ADD(p, -3)
-#define B_SETSIZE(b,s) (B_ACCESS(b, 0) = (s))
-#define B_SETNEXT(b,n) (B_ACCESS(b, 1) = (n))
-#define B_SETPREV(b,n) (B_ACCESS(b, 2) = (n))
-#define B_NBLOCKS(n_bytes) ((((uint64_t)(n_bytes)) + sizeof(tbl)) / sizeof(tbl))
+typedef uint64_t tbr;
+typedef tbr* tbl;
+#define B_SIZE(b) *(tbl)(b)
+#define B_NEXT(b) (tbl)(*((tbl)(b)+1))
+#define B_PREV(b) (tbl)(*((tbl)(b)+2))
+#define B_DATA(b) ((tbl)(b)+3)
+#define B_FROM_DATA_PTR(p) ((tbl)(p) - 3)
+#define B_SETSIZE(b,s) (*((tbl)(b)) = (s))
+#define B_SETNEXT(b,n) (*((tbl)(b) + 1) = ((tbr)(n)))
+#define B_SETPREV(b,n) (*((tbl)(b) + 2) = ((tbr)(n)))
+#define B_NBLOCKS(n_bytes) ((((tbr)(n_bytes)) + sizeof(tbl)) / sizeof(tbl))
 
 #define BUF_SZ 0x1000
 #define MIN_ADD_SZ 0x20 /* minimum amount of blocks to add */
@@ -20,13 +19,13 @@ typedef uint64_t tbl;
 
 tbl pbuf[BUF_SZ];
 tbl buf = (tbl) pbuf;
-tbl used;
+tbr used;
 
 void init()
 {
-    B_SIZE(buf) = 0;
-    B_PREV(buf) = buf;
-    B_NEXT(buf) = buf;
+    B_SETSIZE(buf, 0);
+    B_SETPREV(buf, buf);
+    B_SETNEXT(buf, buf);
     used = 3;
 }
 
@@ -63,16 +62,16 @@ void del(tbl b)
 
 /* add memory to current memory, returns index of new segment
  * the memory is initialized */
-tbl add_mem(tbl nblocks)
+tbl add_mem(tbr nblocks)
 {
-    if (nblocks < MIN_ADD_SZ)
+    if ((tbr)nblocks < MIN_ADD_SZ)
         nblocks = MIN_ADD_SZ;
 
     if (used + nblocks < BUF_SZ)
         return 0;
 
     tbl result = (tbl)(&pbuf[used]);
-    used += B_ADD(nblocks, 3); /* add for the header */
+    used += nblocks + 3;
     B_SETSIZE(result, nblocks);
     insert_before(buf, result);
     return result;
@@ -80,14 +79,14 @@ tbl add_mem(tbl nblocks)
 
 /* get data from list, split and remove if needed
  * returns pointer to data */
-void *take(tbl b, tbl sz)
+void *take(tbl b, tbr sz)
 {
     void *data;
-    tbl new_sz = B_SIZE(b) - sz - 3;
+    tbr new_sz = B_SIZE(b) - sz - 3;
     data = (void*)B_DATA(b);
     if (new_sz >= MIN_DATA_SZ)
     {
-        tbl bnew = B_ADD(b, 3 + sz);
+        tbl bnew = b + 3 + sz;
         B_SETSIZE(b, sz);
         insert_after(b, bnew);
     }
@@ -97,10 +96,9 @@ void *take(tbl b, tbl sz)
 
 void *get_mem(int n_bytes)
 {
-    tbl sz = B_NBLOCKS(n_bytes);
+    tbr sz = B_NBLOCKS(n_bytes);
     tbl data;
 
-    /* try to find a block */
     tbl curr = B_NEXT(buf);
     while (curr != buf)
     {
@@ -127,8 +125,8 @@ void try_merge(tbl b1, tbl b2)
     if (b2 == buf)
         return;
 
-    tbl sz1 = B_SIZE(b1);
-    if (B_ADD(B_DATA(b1), sz1) == b2)
+    tbr sz1 = B_SIZE(b1);
+    if (B_DATA(b1) + sz1 == b2)
     {
         B_SETSIZE(b1, sz1 + B_SIZE(b2) + 3);
         del(b2);
